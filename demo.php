4f88778b60e067d6cd6d7b246daa45fbdde9650a
huangweining <jason.huang@livelocity.cn>
2020-10-16 17:07:05 +0800

    地图

--- demo.php
+++ demo.php
@@ -1,4 +1,15 @@
 <?php
 require_once './vendor/autoload.php';
-use \Tools\QRCode;
-QRCode::Hello();
+
+#二维码使用
+//use \Tools\QRCode;
+//QRCode::Hello();
+
+#地图使用
+use Tools\Map;
+$map = new Map();
+//$info = $map->locationInfo();
+//$info = $map->countDistance(22.54,114.05,22,114.4);
+$info = $map->distanceSwitchKM(5555.55);
+
+print_r($info);
